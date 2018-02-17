32aa6e987ab1e2165e0006af62b8a327836081d9
Angelo Cigna <acigna1982@gmail.com>
2018-02-17 13:14:56 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
   if board.index == nil
-    return false
   elsif board.index == "O" || board.index == "X"
-    return true
 
 end
 end
