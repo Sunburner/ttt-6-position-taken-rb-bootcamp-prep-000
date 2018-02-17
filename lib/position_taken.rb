0fc0b533df024274ac2a411e3cc4e3da84e326c9
Angelo Cigna <acigna1982@gmail.com>
2018-02-17 13:12:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board,index)
   if board.index == " "
     return true
-  elsif board.index == "O" || board.index == "X"
+  elsif board.index != "O" || board.index == "X"
     return true
   else
     return false
